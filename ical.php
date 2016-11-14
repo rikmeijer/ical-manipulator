ec8ec9c7d4027dde704c5c7e8d4f9ffb73da8080
Rik Meijer <rikmeijer@gmail.com>
2016-11-14 21:07:21 +0100

    add tool that reads a file/url and prints a selection (by date)

--- ical.php
+++ ical.php
@@ -2,14 +2,42 @@
 
 require __DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
-function error(string $message) {
-    print $message;
-    exit(PHP_EOL);
+function println(string $message) {
+    fwrite(STDOUT, $message . PHP_EOL);
 }
 
-if ($_SERVER['argc'] < 2) {
-    error('Please enter at least 1 argument');
+function debug(string $message) {
+    fwrite(STDERR, $message . PHP_EOL);
+    
 }
+
+function error(string $message) {
+    debug($message);
+    exit;
+}
+
+function parseDate(string $date) {
+    return new DateTime($date);
+}
+
+if ($_SERVER['argc'] < 4) {
+    error('Please enter at least 2 arguments (path, startdate)');
+}
+
 
 $vcalendar = Sabre\VObject\Reader::read(fopen($_SERVER['argv'][1],'r'));
-echo $vcalendar->VEVENT->SUMMARY;
+
+$startdate = parseDate($_SERVER['argv'][2]);
+$enddate = parseDate($_SERVER['argv'][3]);
+debug("Printing all events where startdate between " . $startdate->format("d/m/Y") . " and " . $enddate->format("d/m/Y"));
+
+foreach ($vcalendar->VEVENT as $event) {
+    if ($event->DTSTART->getDateTime() < $startdate) {
+        continue;
+    } elseif ($event->DTSTART->getDateTime() > $enddate) {
+        continue;
+    }
+
+    println(Sabre\VObject\Writer::write($event));
+}
+
